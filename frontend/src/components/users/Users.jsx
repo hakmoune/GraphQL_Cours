c75596ab8f506d120495624241e49fd684016d60
hakmoune <mehdi.hakmoune@gmail.com>
2024-09-10 22:43:57 +0200

    Polling, NetworkStatus, Caching in localstorage

--- frontend/src/components/users/Users.jsx
+++ frontend/src/components/users/Users.jsx
@@ -1,10 +1,14 @@
-import { useQuery } from "@apollo/client";
+import { useQuery, NetworkStatus } from "@apollo/client";
 import { QUERY_ALL_USERS } from "../../graphql/queries/usersQueries";
 import User from "./User";
 
 export default function Users() {
-  const { loading, error, data } = useQuery(QUERY_ALL_USERS);
+  const { loading, error, data, networkStatus } = useQuery(QUERY_ALL_USERS, {
+    pollInterval: 100000,
+    notifyOnNetworkStatusChange: true,
+  });
 
+  if (networkStatus === NetworkStatus.poll) return <div>Refetching...</div>;
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
 
--- frontend/src/graphql/config.js
+++ frontend/src/graphql/config.js
@@ -1,6 +1,15 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { persistCache } from "apollo-cache-persist";
+
+const cache = new InMemoryCache();
+
+// Persist the cache to localStorage
+persistCache({
+  cache,
+  storage: window.localStorage, // Use localStorage for persistence
+});
 
 export const client = new ApolloClient({
+  cache,
   uri: "http://localhost:4000/", // Connect with Apollo Server
-  cache: new InMemoryCache(),
 });
